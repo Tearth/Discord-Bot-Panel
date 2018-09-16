c225d0ed630fba09a643cd4fe2ccf297ebe8e2bb
tearth <Tearth@users.noreply.github.com>
2018-09-16 14:29:04 +0200

    Add time provider.

---
+++ DiscordBotPanel/DiscordBotPanel.Backend/Helpers/Time/ITimeProvider.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DiscordBotPanel.Backend.Helpers.Time
+{
+    public interface ITimeProvider
+    {
+        DateTime Get();
+    }
+}
