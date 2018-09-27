0b02d2cb32d7efa247e6c320d8e4c4197f90e5ae
tearth <Tearth@users.noreply.github.com>
2018-09-27 18:10:32 +0200

    Add create time to stats DTO.

--- DiscordBotPanel/DiscordBotPanel.Backend/DTO/LogStatsDTO.cs
+++ DiscordBotPanel/DiscordBotPanel.Backend/DTO/LogStatsDTO.cs
@@ -1,8 +1,11 @@
-﻿namespace DiscordBotPanel.Backend.DTO
+﻿using System;
+
+namespace DiscordBotPanel.Backend.DTO
 {
     public class LogStatsDto
     {
         public ulong BotId { get; set; }
+        public DateTime CreateTime { get; set; }
         public int GuildsCount { get; set; }
         public int MembersCount { get; set; }
         public int ExecutedCommandsCount { get; set; }
