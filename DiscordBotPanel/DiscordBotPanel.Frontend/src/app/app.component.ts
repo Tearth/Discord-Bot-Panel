1b681b5ad653117884ccd1970cf4db8f96844e34
tearth <Tearth@users.noreply.github.com>
2018-09-28 22:07:15 +0200

    Add library to draw charts.

--- DiscordBotPanel/DiscordBotPanel.Frontend/src/app/app.component.ts
+++ DiscordBotPanel/DiscordBotPanel.Frontend/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Frontend';
+  chartOptions = {
+    responsive: true
+  };
+
+  chartData = [
+    { data: [330, 600, 260, 700], label: 'Account A' },
+    { data: [120, 455, 100, 340], label: 'Account B' },
+    { data: [45, 67, 800, 500], label: 'Account C' }
+  ];
+
+  chartLabels = ['January', 'February', 'Mars', 'April'];
+
+  onChartClick(event) {
+    console.log(event);
+  }
 }
