2dce84686eb40964e47c42eae4dd94a9751aa0e1
tearth <Tearth@users.noreply.github.com>
2018-09-30 13:28:52 +0200

    Add api service.

---
+++ DiscordBotPanel/DiscordBotPanel.Frontend/src/app/services/api.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+    private apiUrl: string = "http://localhost:4000/";
+
+    constructor(private http: HttpClient) {
+
+    }
+
+    public get<T>(endpoint: string) : Observable<T> {
+        return this.http.get<T>(this.apiUrl + endpoint);
+    }
+
+    public post<T>(endpoint: string, payload: string) : Observable<T> {
+        return this.http.post<T>(this.apiUrl + endpoint, payload);
+    }
+}
